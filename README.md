a2c7b88f197abf5a6fa46bb8f46a00bbb2f1107a
Vanilla <solmarians05@gmail.com>
2020-04-20 12:57:06 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-<h1> This is a script that automatically writes the text on TypeRacer.com </h1>
-# Typeracer-Hack
-# Typeracer-Hack
-# Typeracer-Hack
-# Typeracer-Hack
+<h2> This is a script that automatically writes the text on TypeRacer.com </h2>
+<h3> To use the hack, all you need to do is to paste the code in the typeracer console after the race started </h3>
---
+++ script.js
@@ -0,0 +1,30 @@
+var input = document.getElementsByClassName("txtInput")[0];
+var element = document.getElementsByClassName("txtInput")[0];
+var nodes = document.getElementsByClassName("inputPanel")[0].childNodes;
+console.log(nodes[0].innerText);
+
+input.select(); // you can also use input.focus()
+input.value=""; 
+ 
+var text = nodes[0].innerText;
+text = text.substring(0, text.length - 25);
+var now = "a";
+setTimeout(function(){write_text()},time);
+var l=text.length; 
+var current = 0; 
+var time = 240; 
+
+var write_text = function() {
+  input.value+=text[current]; 
+  if (text[current] == ' ' || text[current] == '.'){
+  	element.dispatchEvent(new KeyboardEvent('keydown',{'key':'Shift'}));
+  	element.dispatchEvent(new KeyboardEvent('keyup',{'key':'Shift'}));
+  }
+  if(current < l-1) { 
+    current++; 
+    setTimeout(function(){write_text()},time); 
+  } 
+} 
+setTimeout(function(){write_text()},time);
+element.dispatchEvent(new KeyboardEvent('keydown',{'key':'Shift'}));
+element.dispatchEvent(new KeyboardEvent('keyup',{'key':'Shift'}));
